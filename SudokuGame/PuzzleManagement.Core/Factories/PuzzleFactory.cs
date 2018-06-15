227d0171a4a52ba1fc0685358ea2d8ba4c673880
Michael Moody <dcmoods@gmail.com>
2018-06-15 10:57:06 -0400

    Added Factory and tests

---
+++ SudokuGame/PuzzleManagement.Core/Factories/PuzzleFactory.cs
@@ -0,0 +1,28 @@
+﻿using PuzzleManagement.Core.Enums;
+using PuzzleManagement.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuzzleManagement.Core.Factories
+{
+    public class PuzzleFactory
+    {
+        public static Puzzle GetPuzzle(Difficulty difficulty)
+        {
+            switch (difficulty)
+            {
+                case Difficulty.Easy:
+                    return new EasyPuzzle();
+                case Difficulty.Medium:
+                    return new MediumPuzzle();
+                case Difficulty.Hard:
+                    return new HardPuzzle();
+                default:
+                    return new EmptyPuzzle();
+            }
+        }
+    }
+}
---
+++ SudokuGame/PuzzleManagement.Tests/Factory/FactoryTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PuzzleManagement.Core.Factories;
+using PuzzleManagement.Core.Models;
+
+namespace PuzzleManagement.Tests.Factory
+{
+    [TestClass]
+    public class FactoryTests
+    {
+        [TestMethod]
+        public void ReturnEmptyPuzzleByDefault()
+        {
+            var puzzle = PuzzleFactory.GetPuzzle(Core.Enums.Difficulty.Empty);
+            Assert.IsInstanceOfType(puzzle, typeof(EmptyPuzzle));
+        }
+
+        [TestMethod]
+        public void ReturnsEasyPuzzle()
+        {
+            var puzzle = PuzzleFactory.GetPuzzle(Core.Enums.Difficulty.Easy);
+            Assert.IsInstanceOfType(puzzle, typeof(EasyPuzzle));
+        }
+
+        [TestMethod]
+        public void ReturnsMediumPuzzle()
+        {
+            var puzzle = PuzzleFactory.GetPuzzle(Core.Enums.Difficulty.Medium);
+            Assert.IsInstanceOfType(puzzle, typeof(MediumPuzzle));
+        }
+
+        [TestMethod]
+        public void ReturnsHardPuzzle()
+        {
+            var puzzle = PuzzleFactory.GetPuzzle(Core.Enums.Difficulty.Hard);
+            Assert.IsInstanceOfType(puzzle, typeof(HardPuzzle));
+        }
+    }
+}
